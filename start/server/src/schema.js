c59aef186c989570158cf2aacf1b17455ec3b9ee
osamakhanedu <osamakhanedu@gmail.com>
2020-02-24 19:42:26 +0500

    Server Schema update

---
+++ start/server/src/schema.js
@@ -0,0 +1,53 @@
+const { gql } =  require('apollo-server');
+
+const typeDefs = gql`
+  # schema goes here.
+  type Launch {
+    id: ID!
+    site: String
+    missioin: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  
+  type Rocket {
+   id: ID!
+   name: String
+   type: String	  
+  }
+
+  type User {
+  id: ID!
+  email: String!
+  trips:[Launch]!
+ }
+  type Mission {
+   name: String
+   missinPath(size: PatchSize): String
+  }
+
+ enum PatchSize {
+  SMALL
+  LARGE
+ }
+
+ type TripUpdateResponse {
+  success: Boolean!
+  message: String
+  launches: [Launch]
+}
+
+type Query {
+  launches: [Launch]!
+  launch(id: ID!): Launch
+  me: User
+}
+ 
+ type Mutation {
+  bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  cancelTrip(launchId: ID!): TripUpdateResponse!
+  login(email: String): String # login token
+}
+`
+
+module.exports = typeDefs;
