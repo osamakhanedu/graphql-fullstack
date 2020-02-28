d47c881b2d19a6daa278140762286c455164deb3
osamakhanedu <osamakhanedu@gmail.com>
2020-02-28 20:19:10 +0500

    Data Source added

--- start/server/src/datasources/API.js
+++ start/server/src/datasources/API.js
@@ -13,6 +13,19 @@ class LaunchAPI extends RESTDataSource {
     : [];
     }
 
+    async getLaunchById({ launchId }) {
+        const response = await this.get('launches', { flight_number: launchId });
+        return this.launchReducer(response[0]);
+      }
+      
+
+      getLaunchesByIds({ launchIds }) {
+        return Promise.all(
+          launchIds.map(launchId => this.getLaunchById({ launchId })),
+        );
+      }
+
+
     launchReducer(launch) {
         return {
             id: launch.flight_number || 0,
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,6 +2,13 @@ const { gql } =  require('apollo-server');
 
 const typeDefs = gql`
   # schema goes here.
+
+  type User {
+    id: ID!
+    email: String!
+    trips:[Launch]!
+   }
+
   type Launch {
     id: ID!
     site: String
@@ -16,11 +23,7 @@ const typeDefs = gql`
    type: String	  
   }
 
-  type User {
-  id: ID!
-  email: String!
-  trips:[Launch]!
- }
+  
   type Mission {
    name: String
    missinPath(size: PatchSize): String
